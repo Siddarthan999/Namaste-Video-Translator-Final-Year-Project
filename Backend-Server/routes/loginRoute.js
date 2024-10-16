f7ab7f9694c50e26a9286d785955cd16681baa47
Siddarthan <91734840+Siddarthan999@users.noreply.github.com>
2024-10-16 21:02:21 +0530

    Delete Backend-Server directory

--- Backend-Server/routes/loginRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const {authenticateUser} = require('../controllers/loginController')
-
-router.get('/:email/:password', authenticateUser);
-
-module.exports = router
--- Backend-Server/routes/signupRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const addNewUser = require('../controllers/signupController')
-router.post('/', addNewUser)
-
-module.exports = router
