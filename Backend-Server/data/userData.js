b48d4f2897ade9aec3519bad8aaaaed8ad66e973
Siddarthan <91734840+Siddarthan999@users.noreply.github.com>
2024-11-19 21:10:46 +0530

    v1

---
+++ Backend-Server/data/userData.js
@@ -0,0 +1,11 @@
+const userData = [
+    {
+        "firstName": "Siddarthan",
+        "lastName": "Natarajan",
+        "phoneNumber": "1234567890",
+        "email": "sidd@gmail.com",
+        "password": "sidd"
+    }  
+];
+
+module.exports = userData
---
+++ Backend-Server/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        collection: 'NamasteLoginDataCollection'
+    }
+);
+
+module.exports = mongoose.model('NamasteLoginDataCollection', userSchema);
